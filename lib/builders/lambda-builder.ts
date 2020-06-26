9d1b9adc08fd0ffb31a64cd12dbdf1c2ea6eb6b2
waltermanias <wmanias@gmail.com>
2020-06-26 20:32:49 -0300

    Update lambda function code

--- lib/builders/lambda-builder.ts
+++ lib/builders/lambda-builder.ts
@@ -1,5 +1,5 @@
 import * as cdk from '@aws-cdk/core'
-import { Function, Runtime, Code } from '@aws-cdk/aws-lambda'
+import { Function, Runtime, Code, LayerVersion } from '@aws-cdk/aws-lambda'
 import { Role, ServicePrincipal, ManagedPolicy } from '@aws-cdk/aws-iam'
 import * as path from 'path'
 
@@ -7,6 +7,12 @@ export class LambdaBuilder {
   constructor(private scope: cdk.Construct, private props?: cdk.StackProps) {}
 
   build(): Function {
+    const layer = new LayerVersion(this.scope, 'HTMLToPDFLambdaLayer', {
+      code: Code.fromAsset(path.join(__dirname, '../lambda-layer')),
+      description: 'Layer to support pdf converter.',
+      layerVersionName: 'HTMLToPDFLambdaLayer',
+    })
+
     const lambda = new Function(this.scope, 'Lambda', {
       functionName: 'VerifyBreachedMailAccountFunction',
       runtime: Runtime.NODEJS_12_X,
@@ -18,9 +24,11 @@ export class LambdaBuilder {
       role: new Role(this.scope, 'LambdaRole', {
         roleName: 'VerifyBreachedMailAccountRole',
         path: '/',
-        managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaRole')],
+        managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole')],
         assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
       }),
+      layers: [layer],
+      timeout: cdk.Duration.seconds(10),
     })
     return lambda
   }
